d5c2c34d0bf9287add5aa3f54af012ae3785939b
Vastav07 <vastavbharambe@gmail.com>
2023-06-12 16:22:40 -0400

    Modified the files according to the latest updates

--- background.js
+++ background.js
@@ -1,13 +1,113 @@
+// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+//   if (request.action === "getRecommendedVideos") {
+//     const query = request.query.replace(/^Query:\s*/, "");
+//     console.log("Query:", query); // Log the query to the console
+
+//     const apiKey = "AIzaSyBNnJJ7Pv-fChJU2PzRh_9CSUFJayTYooQ";
+//     const newsApiKey = "c7f994db99b04ee28269699753c6056c";
+//     const BookApi = "AIzaSyBNnJJ7Pv-fChJU2PzRh_9CSUFJayTYooQ";
+//     const tmdb = "b38f8219d323448fbcc5c3ee11ecad42";
+//     const ieeeApiKey = "4ktm3rsf4t3d6u4rn3yjcyvu";
+
+//     // fetch random videos, articles, books
+//     Promise.allSettled([
+//       fetch(`https://www.googleapis.com/youtube/v3/search?key=${apiKey}&part=snippet&type=video&q=${query}`)
+//       .then((response) => response.json()),
+//       fetch(`https://newsapi.org/v2/everything?q=${query}&apiKey=${newsApiKey}`)
+//       .then((response) => response.json()),
+//       fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}&key=${BookApi}`)
+//       .then((response) => response.json()),
+//       fetch(`https://api.themoviedb.org/3/search/movie?api_key=${tmdb}&query=${query}`)
+//         .then((response) => response.json()),
+//       // fetch(`https://ieeexploreapi.ieee.org/api/v1/search/articles?apikey=${ieeeApiKey}&querytext=${query}`)
+//       //   .then((response) => response.json())
+//     ])
+//     .then((results) => {
+//       console.log("videos", results[0].value?.items)
+//       console.log("articles", results[1].value?.articles)
+//       console.log("books", results[2].value?.items)
+//       console.log("movies", results[3].value?.results);
+//       // console.log("publications", results[4].value?.articles);
+    
+
+
+//       const videos = results[0].value.items?.map((item) => {
+//         return {
+//           title: item.snippet.title,
+//           thumbnail: item.snippet.thumbnails.default.url,
+//           videoId: item.id.videoId,
+//         };
+//       }) || [];
+//       shuffleArray(videos);
+//       const recommendedVideos = videos.slice(0, 3);
+
+
+//       // only first 5 articles
+//       const articles = results[1].value.articles?.slice(0, 2).map((article) => {
+//         return {
+//           title: article.title,
+//           description: article.description,
+//           url: article.url,
+//           imageUrl: article.urlToImage,
+//         };
+//       }) || [];
+      
+//     // top 5 books
+//       const books = results[2].value.items?.slice(0, 1).map((item) => {
+//         return {
+//           title: item.volumeInfo.title,
+//           thumbnail: item.volumeInfo.imageLinks?.thumbnail || "https://via.placeholder.com/128x192.png?text=No+Cover",
+//           bookId: item.id,
+//           url: item.volumeInfo.infoLink,
+//         };
+//       });
+
+//       const movies = results[3].value?.results?.slice(0, 1).map((movie) => {
+//         return {
+//           title: movie.title,
+//           thumbnail: movie.poster_path ? `https://image.tmdb.org/t/p/w200${movie.poster_path}` : "https://via.placeholder.com/200x300.png?text=No+Poster",
+//           movieId: movie.id,
+//           url: `https://www.themoviedb.org/movie/${movie.id}`,
+//         };
+//       });
+
+//       // const publications = results[4].value?.articles?.map((publication) => {
+//       //   return {
+//       //     title: publication.title,
+//       //     description: publication.abstract || publication.articleNumber,
+//       //     url: publication.pdfUrl || publication.htmlUrl,
+//       //   };
+//       // }) || [];
+
+//       sendResponse({ videos: recommendedVideos, articles, books, movies, query });
+//   })
+//     .catch((error) => {
+//       console.error("Error fetching recommended videos:", error);
+//       sendResponse({ error: "Failed to fetch recommended videos" });
+//     });
+
+//   function shuffleArray(array) {
+//     for (let i = array.length - 1; i > 0; i--) {
+//       const j = Math.floor(Math.random() * (i + 1));
+//       [array[i], array[j]] = [array[j], array[i]];
+//     }
+//   }
+//     // Return true to indicate that the sendResponse function will be called asynchronously
+//     return true;
+//   }
+// });
+
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.action === "getRecommendedVideos") {
     const query = request.query.replace(/^Query:\s*/, "");
-    console.log("Query:", query); // Log the query to the console
+    // console.log("Query:", query); // Log the query to the console
 
-    const apiKey = "AIzaSyBNnJJ7Pv-fChJU2PzRh_9CSUFJayTYooQ";
+    const apiKey = "AIzaSyDFl3gMtA6rl8UyvBKD2gOpMFUw9wVvoIs";
     const newsApiKey = "c7f994db99b04ee28269699753c6056c";
     const BookApi = "AIzaSyBNnJJ7Pv-fChJU2PzRh_9CSUFJayTYooQ";
     const tmdb = "b38f8219d323448fbcc5c3ee11ecad42";
     const ieeeApiKey = "4ktm3rsf4t3d6u4rn3yjcyvu";
+    const searchkey = "f053ecf0867b93ab982aea4bf0e1177099865234d4472e78a282263ebc6456c8";
 
     // fetch random videos, articles, books
     Promise.allSettled([
@@ -21,12 +121,15 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
         .then((response) => response.json()),
       // fetch(`https://ieeexploreapi.ieee.org/api/v1/search/articles?apikey=${ieeeApiKey}&querytext=${query}`)
       //   .then((response) => response.json())
+      fetch(`https://serpapi.com/search.json?engine=google&q=${query}&google_domain=google.com&hl=en&api_key=${searchkey}`)
+      .then((response) => response.json()),
     ])
     .then((results) => {
       console.log("videos", results[0].value?.items)
-      console.log("articles", results[1].value?.articles)
+      // console.log("articles", results[1].value?.articles)
       console.log("books", results[2].value?.items)
-      console.log("movies", results[3].value?.results);
+      // console.log("movies", results[3].value?.results);
+      console.log('links', results[4].value?.organic_results);
       // console.log("publications", results[4].value?.articles);
     
 
@@ -43,14 +146,14 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 
 
       // only first 5 articles
-      const articles = results[1].value.articles?.slice(0, 2).map((article) => {
-        return {
-          title: article.title,
-          description: article.description,
-          url: article.url,
-          imageUrl: article.urlToImage,
-        };
-      }) || [];
+      // const articles = results[1].value.articles?.slice(0, 2).map((article) => {
+      //   return {
+      //     title: article.title,
+      //     description: article.description,
+      //     url: article.url,
+      //     imageUrl: article.urlToImage,
+      //   };
+      // }) || [];
       
     // top 5 books
       const books = results[2].value.items?.slice(0, 1).map((item) => {
@@ -60,17 +163,27 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
           bookId: item.id,
           url: item.volumeInfo.infoLink,
         };
-      });
+      })|| [];
 
-      const movies = results[3].value?.results?.slice(0, 1).map((movie) => {
+      // const movies = results[3].value?.results?.slice(0, 1).map((movie) => {
+      //   return {
+      //     title: movie.title,
+      //     thumbnail: movie.poster_path ? `https://image.tmdb.org/t/p/w200${movie.poster_path}` : "https://via.placeholder.com/200x300.png?text=No+Poster",
+      //     movieId: movie.id,
+      //     url: `https://www.themoviedb.org/movie/${movie.id}`,
+      //   };
+      // });
+      const links = results[4].value?.organic_results?.slice(0, 3).map((result) => {
+        console.log(result)
         return {
-          title: movie.title,
-          thumbnail: movie.poster_path ? `https://image.tmdb.org/t/p/w200${movie.poster_path}` : "https://via.placeholder.com/200x300.png?text=No+Poster",
-          movieId: movie.id,
-          url: `https://www.themoviedb.org/movie/${movie.id}`,
+          // title: result.title,
+          url: result.link,
+          title: result.title,
+          
+          
         };
-      });
-
+      
+      })|| [];
       // const publications = results[4].value?.articles?.map((publication) => {
       //   return {
       //     title: publication.title,
@@ -79,7 +192,7 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
       //   };
       // }) || [];
 
-      sendResponse({ videos: recommendedVideos, articles, books, movies, query });
+      sendResponse({ videos: recommendedVideos, books,links, query });
   })
     .catch((error) => {
       console.error("Error fetching recommended videos:", error);
--- popup.js
+++ popup.js
@@ -123,46 +123,255 @@
 //   });
 // });
 
+// document.addEventListener("DOMContentLoaded", function () {
+//   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+//     const currentTab = tabs[0];
+//     if (!currentTab || !currentTab.title) {
+//       console.error("Unable to retrieve tab title.");
+//       return;
+//     }
+//     const pageTitle = currentTab.title;
+//     const url = new URL(currentTab.url);
+//     const urlKeywords = url.hostname.split('.').slice(1, -1).join(' ');
+//     const pathKeywords = url.pathname.split('/').filter(Boolean).join(' ');
+//     const sQuery = pageTitle.replace(/^\(\d+\)\s*/, "").split(" - YouTube")[0];
+//     let searchQuery = sQuery.replace(/ - .*$/, "") + " " + urlKeywords + " " + pathKeywords;
+    
+//     // Apply regex to modify searchQuery
+   
+//     const words = searchQuery.trim().split(" ");
+//     if (words.length >= 2 && /^(google|youtube)$/i.test(words[words.length - 2])) {
+//       words.splice(words.length - 2, 2);
+//     } else if (words.length >= 1 && /^(google|youtube)$/i.test(words[words.length - 1])) {
+//       words.splice(words.length - 1, 1);
+//     }
+    
+//     // Check if the last word is "results" and remove it
+//     if (words[words.length - 1].toLowerCase() === "results") {
+//       words.splice(words.length - 1, 1);
+//     }
+    
+//     searchQuery = words.join(" ").trim();
+    
+//     console.log(searchQuery);
+    
+
+
+
+//     chrome.runtime.sendMessage(
+//       { action: "getRecommendedVideos", query: searchQuery },
+//       function (response) {
+//         if (chrome.runtime.lastError) {
+//           console.error(chrome.runtime.lastError);
+//           return;
+//         }
+
+//         if (response && response.videos) {
+//           const videos = response.videos;
+//           const videoList = document.getElementById("videoList");
+//           videoList.innerHTML = "";
+
+//           const queryElement = document.createElement("p");
+//           queryElement.textContent = searchQuery;
+//           videoList.appendChild(queryElement);
+
+//           videos.forEach((video) => {
+//             const listItem = document.createElement("li");
+
+//             const title = document.createElement("h3");
+//             const videoLink = document.createElement("a");
+//             videoLink.textContent = video.title;
+//             videoLink.href = `https://www.youtube.com/watch?v=${video.videoId}`;
+//             videoLink.target = "_blank";
+
+//             title.appendChild(videoLink);
+
+//             const thumbnail = document.createElement("img");
+//             thumbnail.src = video.thumbnail;
+
+//             listItem.appendChild(title);
+//             listItem.appendChild(thumbnail);
+
+//             videoList.appendChild(listItem);
+//           });
+//         }
+//         if (response && response.articles) {
+//           const articles = response.articles;
+//           const articlesList = document.getElementById("articlesList");
+//           articlesList.innerHTML = "";
+
+//           const queryElement = document.createElement("p");
+//           queryElement.textContent = searchQuery;
+//           articlesList.appendChild(queryElement);
+
+//           if (articles.length === 0) {
+//             const noArticles = document.createElement("p");
+//             noArticles.textContent = "No articles found.";
+//             articlesList.appendChild(noArticles);
+//           }
+
+//           articles.forEach((article) => {
+//             const listItem = document.createElement("li");
+
+//             const title = document.createElement("h3");
+//             const articleLink = document.createElement("a");
+//             articleLink.textContent = article.title;
+//             articleLink.href = article.url;
+//             articleLink.target = "_blank";
+
+//             title.appendChild(articleLink);
+
+//             const thumbnail = document.createElement("img");
+//             thumbnail.src = article.imageUrl;
+
+//             listItem.appendChild(title);
+//             listItem.appendChild(thumbnail);
+
+//             articlesList.appendChild(listItem);
+//           });
+//         }
+//         if (response && response.books) {
+//           const books = response.books;
+//           const booksList = document.getElementById("booksList");
+//           booksList.innerHTML = "";
+
+//           const queryElement = document.createElement("p");
+//           queryElement.textContent = searchQuery;
+//           booksList.appendChild(queryElement);
+
+//           if (books.length === 0) {
+//             const noBooks = document.createElement("p");
+//             noBooks.textContent = "No books found.";
+//             booksList.appendChild(noBooks);
+//           }
+
+//           books.forEach((book) => {
+//             const listItem = document.createElement("li");
+
+//             const title = document.createElement("h3");
+//             const bookLink = document.createElement("a");
+//             bookLink.textContent = book.title;
+//             bookLink.href = book.url;
+//             bookLink.target = "_blank";
+
+//             title.appendChild(bookLink);
+
+//             const thumbnail = document.createElement("img");
+//             thumbnail.src = book.thumbnail;
+
+//             listItem.appendChild(title);
+//             listItem.appendChild(thumbnail);
+
+//             booksList.appendChild(listItem);
+//           });
+//         }
+//         if (response && response.movies) {
+//           const movies = response.movies;
+//           const moviesList = document.getElementById("moviesList");
+//           moviesList.innerHTML = "";
+        
+//           const queryElement = document.createElement("p");
+//           queryElement.textContent = response.query;
+//           moviesList.appendChild(queryElement);
+        
+//           if (movies.length === 0) {
+//             const noMovies = document.createElement("p");
+//             noMovies.textContent = "No movies found.";
+//             moviesList.appendChild(noMovies);
+//           }
+        
+//           movies.forEach((movie) => {
+//             const listItem = document.createElement("li");
+        
+//             const title = document.createElement("h3");
+//             const movieLink = document.createElement("a");
+//             movieLink.textContent = movie.title;
+//             movieLink.href = `https://www.themoviedb.org/movie/${movie.movieId}`;
+//             movieLink.target = "_blank";
+        
+//             title.appendChild(movieLink);
+        
+//             const thumbnail = document.createElement("img");
+//             thumbnail.src = movie.thumbnail;
+        
+//             listItem.appendChild(title);
+//             listItem.appendChild(thumbnail);
+        
+//             moviesList.appendChild(listItem);
+//           });
+//         }
+//         // if (response && response.publications) {
+//         //   const publications = response.publications;
+//         //   const publicationsList = document.getElementById("publicationsList");
+//         //   publicationsList.innerHTML = "";
+
+//         //   const queryElement = document.createElement("p");
+//         //   queryElement.textContent = searchQuery;
+//         //   publicationsList.appendChild(queryElement);
+
+//         //   if (publications.length === 0) {
+//         //     const noPublications = document.createElement("p");
+//         //     noPublications.textContent = "No publications found.";
+//         //     publicationsList.appendChild(noPublications);
+//         //   }
+
+//         //   publications.forEach((publication) => {
+//         //     const listItem = document.createElement("li");
+
+//         //     const title = document.createElement("h3");
+//         //     const publicationLink = document.createElement("a");
+//         //     publicationLink.textContent = publication.title;
+//         //     publicationLink.href = publication.url;
+//         //     publicationLink.target = "_blank";
+
+//         //     title.appendChild(publicationLink);
+
+//         //     listItem.appendChild(title);
+
+//         //     publicationsList.appendChild(listItem);
+//         //   });
+//         // }
+//       });
+//   });
+// }); 
+
+
+
 document.addEventListener("DOMContentLoaded", function () {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     const currentTab = tabs[0];
-    if (!currentTab || !currentTab.title) {
-      console.error("Unable to retrieve tab title.");
+    if (!currentTab || !currentTab.title || !currentTab.url.startsWith("http")) {
+      displayErrorMessage("I can't help you.");
       return;
     }
+
     const pageTitle = currentTab.title;
     const url = new URL(currentTab.url);
     const urlKeywords = url.hostname.split('.').slice(1, -1).join(' ');
     const pathKeywords = url.pathname.split('/').filter(Boolean).join(' ');
     const sQuery = pageTitle.replace(/^\(\d+\)\s*/, "").split(" - YouTube")[0];
     let searchQuery = sQuery.replace(/ - .*$/, "") + " " + urlKeywords + " " + pathKeywords;
-    
+
     // Apply regex to modify searchQuery
-   
     const words = searchQuery.trim().split(" ");
     if (words.length >= 2 && /^(google|youtube)$/i.test(words[words.length - 2])) {
       words.splice(words.length - 2, 2);
     } else if (words.length >= 1 && /^(google|youtube)$/i.test(words[words.length - 1])) {
       words.splice(words.length - 1, 1);
     }
-    
     // Check if the last word is "results" and remove it
     if (words[words.length - 1].toLowerCase() === "results") {
       words.splice(words.length - 1, 1);
     }
-    
     searchQuery = words.join(" ").trim();
-    
-    console.log(searchQuery);
-    
-
-
+    // console.log(searchQuery);
 
     chrome.runtime.sendMessage(
       { action: "getRecommendedVideos", query: searchQuery },
       function (response) {
         if (chrome.runtime.lastError) {
           console.error(chrome.runtime.lastError);
+          displayErrorMessage("Failed to fetch recommended videos");
           return;
         }
 
@@ -171,73 +380,52 @@ document.addEventListener("DOMContentLoaded", function () {
           const videoList = document.getElementById("videoList");
           videoList.innerHTML = "";
 
-          const queryElement = document.createElement("p");
-          queryElement.textContent = searchQuery;
-          videoList.appendChild(queryElement);
+          // const queryElement = document.createElement("p");
+          // queryElement.textContent = searchQuery;
+          // videoList.appendChild(queryElement);
+
+          let videoCount = 0;
+          const currentVideoId = getCurrentVideoId(url);
 
           videos.forEach((video) => {
-            const listItem = document.createElement("li");
+            if (video.videoId !== currentVideoId && videoCount < 3) {
+              const listItem = document.createElement("li");
 
-            const title = document.createElement("h3");
-            const videoLink = document.createElement("a");
-            videoLink.textContent = video.title;
-            videoLink.href = `https://www.youtube.com/watch?v=${video.videoId}`;
-            videoLink.target = "_blank";
+              const title = document.createElement("h3");
+              const videoLink = document.createElement("a");
+              videoLink.textContent = video.title;
+              videoLink.href = `https://www.youtube.com/watch?v=${video.videoId}`;
+              videoLink.target = "_blank";
 
-            title.appendChild(videoLink);
+              title.appendChild(videoLink);
 
-            const thumbnail = document.createElement("img");
-            thumbnail.src = video.thumbnail;
+              const thumbnail = document.createElement("img");
+              thumbnail.src = video.thumbnail;
 
-            listItem.appendChild(title);
-            listItem.appendChild(thumbnail);
+              listItem.appendChild(title);
+              listItem.appendChild(thumbnail);
 
-            videoList.appendChild(listItem);
+              videoList.appendChild(listItem);
+
+              videoCount++;
+            }
           });
-        }
-        if (response && response.articles) {
-          const articles = response.articles;
-          const articlesList = document.getElementById("articlesList");
-          articlesList.innerHTML = "";
 
-          const queryElement = document.createElement("p");
-          queryElement.textContent = searchQuery;
-          articlesList.appendChild(queryElement);
-
-          if (articles.length === 0) {
-            const noArticles = document.createElement("p");
-            noArticles.textContent = "No articles found.";
-            articlesList.appendChild(noArticles);
+          if (videoCount === 0) {
+            const noVideos = document.createElement("p");
+            noVideos.textContent = "No relevant videos found.";
+            videoList.appendChild(noVideos);
           }
-
-          articles.forEach((article) => {
-            const listItem = document.createElement("li");
-
-            const title = document.createElement("h3");
-            const articleLink = document.createElement("a");
-            articleLink.textContent = article.title;
-            articleLink.href = article.url;
-            articleLink.target = "_blank";
-
-            title.appendChild(articleLink);
-
-            const thumbnail = document.createElement("img");
-            thumbnail.src = article.imageUrl;
-
-            listItem.appendChild(title);
-            listItem.appendChild(thumbnail);
-
-            articlesList.appendChild(listItem);
-          });
         }
+
         if (response && response.books) {
           const books = response.books;
           const booksList = document.getElementById("booksList");
           booksList.innerHTML = "";
 
-          const queryElement = document.createElement("p");
-          queryElement.textContent = searchQuery;
-          booksList.appendChild(queryElement);
+          // const queryElement = document.createElement("p");
+          // queryElement.textContent = searchQuery;
+          // booksList.appendChild(queryElement);
 
           if (books.length === 0) {
             const noBooks = document.createElement("p");
@@ -265,72 +453,67 @@ document.addEventListener("DOMContentLoaded", function () {
             booksList.appendChild(listItem);
           });
         }
-        if (response && response.movies) {
-          const movies = response.movies;
-          const moviesList = document.getElementById("moviesList");
-          moviesList.innerHTML = "";
+        if (response && response.links) {
+          const links = response.links;
+          const linksList = document.getElementById("linksList");
+          linksList.innerHTML = "";
         
-          const queryElement = document.createElement("p");
-          queryElement.textContent = response.query;
-          moviesList.appendChild(queryElement);
+          // const queryElement = document.createElement("p");
+          // queryElement.textContent = searchQuery;
+          // linksList.appendChild(queryElement);
         
-          if (movies.length === 0) {
-            const noMovies = document.createElement("p");
-            noMovies.textContent = "No movies found.";
-            moviesList.appendChild(noMovies);
+          const titleElement = document.createElement("h2");
+          titleElement.textContent = "Search Results";
+          linksList.appendChild(titleElement);
+        
+          // const linksHeading = document.createElement("h3");
+          // linksHeading.textContent = "Links";
+          // linksList.appendChild(linksHeading);
+        
+          if (links.length === 0) {
+            const noLinks = document.createElement("p");
+            noLinks.textContent = "No links found.";
+            linksList.appendChild(noLinks);
           }
         
-          movies.forEach((movie) => {
+          links.forEach((link) => {
             const listItem = document.createElement("li");
         
-            const title = document.createElement("h3");
-            const movieLink = document.createElement("a");
-            movieLink.textContent = movie.title;
-            movieLink.href = `https://www.themoviedb.org/movie/${movie.movieId}`;
-            movieLink.target = "_blank";
+            const linkTitle = document.createElement("h4");
+            linkTitle.textContent = link.title;
         
-            title.appendChild(movieLink);
+            const linkElement = document.createElement("a");
+            linkElement.textContent = link.url;
+            linkElement.href = link.url;
+            linkElement.target = "_blank";
         
-            const thumbnail = document.createElement("img");
-            thumbnail.src = movie.thumbnail;
+            listItem.appendChild(linkTitle);
+            listItem.appendChild(linkElement);
         
-            listItem.appendChild(title);
-            listItem.appendChild(thumbnail);
-        
-            moviesList.appendChild(listItem);
+            linksList.appendChild(listItem);
           });
-        }
-        // if (response && response.publications) {
-        //   const publications = response.publications;
-        //   const publicationsList = document.getElementById("publicationsList");
-        //   publicationsList.innerHTML = "";
-
-        //   const queryElement = document.createElement("p");
-        //   queryElement.textContent = searchQuery;
-        //   publicationsList.appendChild(queryElement);
-
-        //   if (publications.length === 0) {
-        //     const noPublications = document.createElement("p");
-        //     noPublications.textContent = "No publications found.";
-        //     publicationsList.appendChild(noPublications);
-        //   }
-
-        //   publications.forEach((publication) => {
-        //     const listItem = document.createElement("li");
-
-        //     const title = document.createElement("h3");
-        //     const publicationLink = document.createElement("a");
-        //     publicationLink.textContent = publication.title;
-        //     publicationLink.href = publication.url;
-        //     publicationLink.target = "_blank";
-
-        //     title.appendChild(publicationLink);
-
-        //     listItem.appendChild(title);
-
-        //     publicationsList.appendChild(listItem);
-        //   });
-        // }
-      });
+        } 
+      }
+    );
   });
-}); 
+});
+
+function getCurrentVideoId(url) {
+  const urlParams = new URLSearchParams(url.search);
+  const vParam = urlParams.get("v");
+  if (vParam) {
+    return vParam;
+  }
+
+  const parts = url.pathname.split("/");
+  if (parts.length >= 2 && parts[1] === "watch") {
+    return parts[2];
+  }
+
+  return null;
+}
+
+function displayErrorMessage(message) {
+  const errorElement = document.getElementById("errorMessage");
+  errorElement.textContent = message;
+}
