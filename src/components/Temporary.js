5453a01265da664f99fd922d6b9aa9f1af84b4a7
zionmelson <92884383+zionmelson@users.noreply.github.com>
2023-06-14 20:27:55 -0400

    style changes

--- src/components/Temporary.js
+++ src/components/Temporary.js
@@ -1,34 +1,46 @@
-import React from 'react';
-import Footer from './Footer';
-import { AiOutlineClose } from 'react-icons/ai';
-import './index.css'
+import React from "react";
+import Footer from "./Footer";
+import { AiOutlineClose } from "react-icons/ai";
+import "./index.css";
 
 const Bookmark = () => {
+  const handleCloseClick = () => {
+    window.close(); // Close the popup window
+  };
 
-    const handleCloseClick = () => {
-        window.close(); // Close the popup window
-    };
-
-    return (
-        <div>
-            <div className="top-container">
-                <div className="logo-container">
-                    <img src={process.env.PUBLIC_URL + '/bear_without_background.png'} alt="bear without background" className="chrome-extension-logo" />
-                    <h1>learnmutiny</h1>
-                </div>
-                <button onClick={handleCloseClick} className="close-popup"><AiOutlineClose className="close-popup" /></button>
-            </div>
-            <div className="bottom-container">
-                <div className="top-container">
-                <h1>There doesn&apos;t seem to be anything on this page I can help you with.</h1>
-                </div>
-                <div className="bookmark-info">
-                <img src={process.env.PUBLIC_URL + '/bear_without_background.png'} alt="bear without background" className="bear-image" />
-                </div>
-            </div>
-            <Footer/>
+  return (
+    <div>
+      <div className="top-container">
+        <div className="logo-container">
+          <img
+            src={process.env.PUBLIC_URL + "/bear_without_background.png"}
+            alt="bear without background"
+            className="chrome-extension-logo"
+          />
+          <h1>learnmutiny</h1>
         </div>
-    );
+        <button onClick={handleCloseClick} className="close-popup">
+          <AiOutlineClose className="close-popup" />
+        </button>
+      </div>
+      <div className="bottom-container">
+        <div className="top-container">
+          <h1>
+            There doesn&apos;t seem to be anything on this page I can help you
+            out with yet.
+          </h1>
+        </div>
+        <div className="bookmark-info">
+          <img
+            src={process.env.PUBLIC_URL + "/bear_without_background.png"}
+            alt="bear without background"
+            className="bear-image"
+          />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default Bookmark;
